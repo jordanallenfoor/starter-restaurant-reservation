e16fbc1ca1dec3bf5f5487228f01c64a7f7eca32
Jordan Foor <jordanallenfoor@gmail.com>
2021-09-14 18:07:30 -0400

    First commit

---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,19 @@
+const knex = require("../db/connection");
+
+// list reservations by date
+
+function list(date) {
+  return knex("reservations").select("*").where({ reservation_date: date });
+}
+
+function create(reservation) {
+  return knex("reservations")
+    .insert(reservation)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+module.exports = {
+  list,
+  create,
+};
